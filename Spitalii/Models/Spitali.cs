54a1f64b1819387a4a0a3f2dfea008df7d73ef4e
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-04-25 23:00:00 +0200

    Crud1

---
+++ Spitalii/Models/Spitali.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Spitalii.Models
+{
+	public class Spitali
+	{
+		public int SpitaliId { get; set; }
+
+		public string SpitalName { get; set; }
+
+		public string City { get; set; }
+	}
+}
+
