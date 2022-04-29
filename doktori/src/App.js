7d5106e9724788a9ad4d6ee042f89844a66e40ca
zeyn13 <likazeynep@gmail.com>
2022-04-29 13:43:04 +0200

    Update Crud

--- doktori/src/App.js
+++ doktori/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import {Home} from './Home';
 import {Doktori} from './Doktori';
 import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
 
@@ -8,16 +7,10 @@ function App() {
   return (
       <BrowserRouter>
     <div className="App container">
-      <h3 className="d-flex justify-content-center m-3">
-        React JS Frontend
-      </h3>
-        
+      
       <nav className="navbar navbar-expand-sm bg-light navbar-dark">
         <ul className="navbar-nav">
           <li className="nav-item- m-1">
-            <NavLink className="btn btn-light btn-outline-primary" to="/home">
-              Home
-            </NavLink>
           </li>
           <li className="nav-item- m-1">
             <NavLink className="btn btn-light btn-outline-primary" to="/Doktori">
@@ -27,7 +20,6 @@ function App() {
         </ul>
       </nav>
       <Routes>
-        <Route exact path='/home' element={<Home/>}/>
         <Route exact path='/Doktori' element={<Doktori/>}/>
       </Routes>
     </div>
