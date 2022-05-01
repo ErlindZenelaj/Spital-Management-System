2d8f94d4543359d31ace05618d47fe22734507ce
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-05-01 19:11:08 +0200

    Crud2(Title)

--- titulli-react/src/App.js
+++ titulli-react/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Home} from './Home';
-import {Spitali} from './Spitali';
+import {Title} from './Title';
 import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
 
 function App() {
@@ -12,21 +11,16 @@ function App() {
       <nav className="navbar navbar-expand-sm bg-light navbar-dark">
         <ul className="navbar-nav">
           <li className="nav-item- m-1">
-            <NavLink className="btn btn-light btn-outline-primary" to="/home">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item- m-1">
-            <NavLink className="btn btn-light btn-outline-primary" to="/Spitali">
-              Spitali
+            <NavLink className="btn btn-light btn-outline-primary" to="/Title">
+              Title
             </NavLink>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route exact path='/home' element={<Home/>}/>
-        <Route exact path='/Spitali' element={<Spitali/>}/>
+
+        <Route exact path='/Title' element={<Title/>}/>
       </Routes>
     </div>
     </BrowserRouter>
---
+++ titulli-react/src/Variables.js
@@ -0,0 +1,3 @@
+export const variables={
+    API_URL:"http://localhost:25135/api/",
+}
