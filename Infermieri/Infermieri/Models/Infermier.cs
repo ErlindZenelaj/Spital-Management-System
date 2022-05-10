7ecf93ada1eff8b7ad1c9a56002a13704c918f63
zeyn13 <likazeynep@gmail.com>
2022-05-10 19:07:27 +0200

    Crud Infermier
    Crud Infermier

---
+++ Infermieri/Infermieri/Models/Infermier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infermieri.Models
+{
+    public class Infermier
+    {
+        public int InfermierId { get; set; }
+
+        public string InfermierName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Age { get; set; }
+
+        public string DoktorName { get; set; }
+    }
+}
