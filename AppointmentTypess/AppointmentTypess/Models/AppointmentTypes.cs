a11625b9be6b35bb6390469afe3f139673b792b9
zeyn13 <likazeynep@gmail.com>
2022-05-19 19:09:00 +0200

    CRUD - Appointment Types
    CRUD - Appointment Types

---
+++ AppointmentTypess/AppointmentTypess/Models/AppointmentTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentTypess.Models
+{
+    public class AppointmentTypes
+    {
+        public int AppointmentTypesId { get; set; }
+
+        public string AppointmentType { get; set; }
+
+        public string UsualAppointmentLength { get; set; }
+
+        public string OnlineBookingAvailable { get; set; }
+    }
+}
---
+++ at-reactt/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import {AppointmentTypes} from './AppointmentTypes';
+import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+      <BrowserRouter>
+    <div className="App container">
+      
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/AppointmentTypes">
+            AppointmentTypes
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path='/AppointmentTypes' element={<AppointmentTypes/>}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+    
+  );
+}
+export default App;
