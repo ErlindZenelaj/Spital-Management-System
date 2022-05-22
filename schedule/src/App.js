348ceebf690cee5abe010515aea8652757db7719
klodiiana <klodianamazrekaj20@gmail.com>
2022-05-22 02:22:46 +0200

    CRUD Schedule

---
+++ schedule/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {Schedule} from './Schedule';
+import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App container">
+        
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/schedule">
+              Schedule
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+      <Route exact path="/schedule" element={<Schedule/>} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
