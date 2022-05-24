06f28f884ac10ef612da1d06748ea824fd402ef8
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-05-24 23:21:28 +0200

    CRUD - Age

---
+++ age-react/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import {Age} from './Age';
+import {BrowserRouter, Route, Routes,NavLink} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App container">
+        
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/Age">
+              Age
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+
+        <Route exact path='/Age' element={<Age/>}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
