d5add39c0651627953267a769df3d748e00afab7
klodiiana <klodianamazrekaj20@gmail.com>
2022-05-24 22:36:50 +0200

    CRUD LaboratoryTest (LlojiTestit)

---
+++ LaboratoryTestss/LaboratoryTestsApp/Models/LabTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratoryTestsApp.Models
+{
+    public class LabTest
+    {
+        public int TestId { get; set; }
+        public string TestName { get; set; }
+        public string Price { get; set; }
+        public string PosNeg { get; set; }
+    }
+}
