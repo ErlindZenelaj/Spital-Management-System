1ce91456bddc54a35ceb0fdecec7eabb386aabeb
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-07-14 20:43:55 +0200

    Api(Authentication)
    Co-Authored-By: zeyn13 <95577898+zeyn13@users.noreply.github.com>
    Co-Authored-By: klodiianamazrekaj <102175182+klodiianamazrekaj@users.noreply.github.com>

---
+++ JwtApp/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "admin", EmailAddress = "admin1@email.com", Password = "ADMIN123", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { Username = "Doctor", EmailAddress = "erlind880@gmail.com", Password = "Doktori2000", GivenName = "Lindi", Surname = "Zenelaj", Role = "Doctor" },
+        };
+    }
+}
